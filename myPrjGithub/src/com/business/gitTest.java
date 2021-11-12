c3003769fb2562275cecc38ae5ce21ea003092a4
Alessandro <Alessandro@LAPTOP-P6GOCAKU.homenet.telecomitalia.it>
2021-11-12 17:37:38 +0100

    final version of today 11/11/2021

--- myPrjGithub/src/com/business/gitTest.java
+++ myPrjGithub/src/com/business/gitTest.java
@@ -2,7 +2,7 @@ package com.business;
 
 public class gitTest {
 	public static void main(String [] args) {
-		System.out.println("base version -1");
+		System.out.println("base version -2");
 	}
 
 }
